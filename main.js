8eab05921fec94678d080fc04c7d44479e6eab2e
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-04-02 16:19:32 -0600

    Algoritmo Sliding Window Completado

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 //Estructura base del campus DEVF
 function findLongestWord(text) {
     // TODO: Dividir el texto en palabras y almacenarlas en una variable
-    // const words = ...
+    const words = text.split(" ");
 
     let longestWord = ''; // Inicializar la palabra más larga
 
     // TODO: Recorrer el arreglo de palabras con un ciclo
-    // for (...) {
+    for (let i = 0; i< words.length; i++) {
         // TODO: Comparar la longitud de la palabra actual con la más larga
-        // if (...) {
-            // Actualizar la palabra más larga
-            // longestWord = ...;
-        // }
-    // }
+        
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];// Actualizar la palabra más larga
+         }
+     }
 
     // TODO: Retornar la palabra más larga encontrada
-    // return ...;
+     return longestWord;
 }
 
 // Ejemplo de uso
 const text = "JavaScript es un lenguaje de programación increíble para aprender.";
 // TODO: Llama a la función y muestra el resultado
+console.log(`La palabra mas larga del texto "${text}" es:`);
+
 console.log(findLongestWord(text)); // Resultado esperado: "programación"
